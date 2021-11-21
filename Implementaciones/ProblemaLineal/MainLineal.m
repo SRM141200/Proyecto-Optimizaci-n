1c6f4e331518a1f316ff893d5d1865e2824c49f4
DavPlazas <72804930+DavPlazas@users.noreply.github.com>
2021-11-21 18:11:21 -0500

    Archivos para la ejecución del problema lineal
    Se debe ejecutar el archivo MainLineal.m

---
+++ Implementaciones/ProblemaLineal/MainLineal.m
@@ -0,0 +1,23 @@
+disp('Scrpit que encuentra la solucion optima dado un Problema Lineal')
+disp('Para realizar un mejor analisis, se comparan las soluciones dado un numero de trabajadores t ingresado')
+disp('0<=t<=6')
+prompt = 'Por favor, ingrese el numero de trabajadores: ';
+y = input(prompt)
+
+while y<0 || y>6
+    disp('Numero ingresado incorrecto')
+    prompt = 'Por favor, ingrese el numero de trabajadores: ';
+    y = input(prompt)
+end
+message=['Se encontrará una solucion optima al problema dado ',num2str(y), ' trabajadores '];
+disp(message)
+
+
+[XB,Z]=CodigoLineal(y);
+
+disp('Solución Optima: ')
+XB
+
+disp('Valor de la funcion Objetivo: ')
+Z
+
